b694d41ac1e15e2190d4d7f21d569e2f6a6b4b1b
unknown <brittanycturner@gmail.com>
2018-03-16 15:53:12 -0700

    working on firebase set vs push

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -11,39 +11,59 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 var playerNumber = 1;
+var playerName = "";
 
 // Send Player's Name to Firebase
 $("#submit").on("click", function() {
   event.preventDefault();
-  var playerName = $("#playerName").val().trim();
-  var playerNumber = 1;
+  playerName = $("#playerName").val().trim();
   var losses1 = 0;
   var losses2 = 0;
   var wins1 = 0;
   var wins2 = 0;
 
   if (playerNumber === 1) {
-  $("#playerNameDiv").html("Hi"+playerName+"! You are Player"+playerNumber)
- 
- database.ref('Players/1').push( {
-    losses: losses1,
-    playerName: playerName,
-    wins: wins1,
-   });
-  playerNumber++;
-
-} else {
-  ("#playerNameDiv").html("Hi"+playerName+"! You are Player"+playerNumber)
-  database.ref('Players/2').push({
-    losses: losses1,
-    playerName: playerName,
-    wins: wins1,
+    $("#playerNameDiv").html("Hi"+playerName+"! You are Player"+playerNumber)
+    database.ref('/players').set( {
+      "test": {
+        losses: losses1,  
+        playerName: playerName,
+        wins: wins1,
+      }
     });
+    
+    console.log(playerNumber);
+
+
+     
+} else {
+  $("#playerNameDiv").html("Hi"+playerName+"! You are Player"+playerNumber)
+  database.ref('/players').push( {
+    "test2": {
+      losses: losses1,  
+      playerName: playerName,
+      wins: wins1,
+    }
+  });
   
 }
 
 });
+
+
+
+//Event Listener for when chid added
+database.ref('/players').on("value", function(snapshot) {
+  console.log (snapshot.val().test.playerName);
   
+  $("#player1").html("Player 1:"+snapshot.val().test.playerName)
+  if (playerNumber === 1) {playerNumber++; }
+  else {playerNumber = 1;}
+
+  //Change text on page, if texton page == xx, then do "player 2"
+});
+
+
 
   //reset game
   //  playerNumber = 1;
